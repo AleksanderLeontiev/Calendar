d9c612142ce36a0846f3d5006f0aef1d95e359f8
aleksandr <aleksandr.leontev.n@mail.ru>
2021-06-26 14:33:26 +0300

    crud

---
+++ src/crud/config.ts
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVjXG_E5JKJJjb7cE7WcrrdaY2CdnUMC8",
+    authDomain: "crubfirebase-2e276.firebaseapp.com",
+    databaseURL: "https://crubfirebase-2e276-default-rtdb.firebaseio.com",
+    projectId: "crubfirebase-2e276",
+    storageBucket: "crubfirebase-2e276.appspot.com",
+    messagingSenderId: "870116999462",
+    appId: "1:870116999462:web:fdbfa744e19d7973b081e7",
+    measurementId: "G-Q0KVV8XQDX"
+};
+firebase.initializeApp(firebaseConfig);
